740e35d9447f9a463b4731bc8e64aeec0ed0921c
TDP17 <ojasinamdar101@gmail.com>
2025-12-15 17:54:07 +0530

    [fix]: spotbugs

--- src/main/java/com/bmc/jenkins/zadviser/model/FailedTestCaseDTO.java
+++ src/main/java/com/bmc/jenkins/zadviser/model/FailedTestCaseDTO.java
@@ -1,11 +1,11 @@
 package com.bmc.jenkins.zadviser.model;
 
 public class FailedTestCaseDTO {
-    public String suiteName;
-    public String testName;
-    public String errorDetails;
-    public String errorStackTrace;
-    public String status;
+    private String suiteName;
+    private String testName;
+    private String errorDetails;
+    private String errorStackTrace;
+    private String status;
 
     public FailedTestCaseDTO(
             String suiteName, String testName, String errorDetails, String errorStackTrace, String status) {
@@ -13,6 +13,46 @@ public class FailedTestCaseDTO {
         this.testName = testName;
         this.errorDetails = errorDetails;
         this.errorStackTrace = errorStackTrace;
+        this.status = status;
+    }
+
+    public String getSuiteName() {
+        return suiteName;
+    }
+
+    public void setSuiteName(String suiteName) {
+        this.suiteName = suiteName;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getErrorDetails() {
+        return errorDetails;
+    }
+
+    public void setErrorDetails(String errorDetails) {
+        this.errorDetails = errorDetails;
+    }
+
+    public String getErrorStackTrace() {
+        return errorStackTrace;
+    }
+
+    public void setErrorStackTrace(String errorStackTrace) {
+        this.errorStackTrace = errorStackTrace;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
     }
 }
